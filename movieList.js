d89ddcae53136a561d6a79f7c88a539e0e3933ec
Ungerdog <jaytunger@gmail.com>
2018-07-01 23:22:26 -0700

    first commit

---
+++ movieList.js
@@ -0,0 +1,22 @@
+
+class MovieList extends React.component {
+
+  constructor (props) {
+    super (props);
+
+    this.state = movie;
+  }
+
+  let movies = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+    ];
+  render () {
+    return <h1>Movie List</>
+           <MovieList 
+
+  }
+}
